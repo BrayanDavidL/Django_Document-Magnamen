055990a98677d645e3f9086b4901daf0c985b22c
BrayanDavidL <brayangarzon474@gmil.com>
2023-11-22 09:17:51 -0500

    Creacion de misgracion y tabla en la base de datos

--- document_management_app/models.py
+++ document_management_app/models.py
@@ -1,3 +1,24 @@
-from django.db import models
+# models.py
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+def document_path(instance, filename):
+    return f'documents/{filename}'
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+class Document(models.Model):
+    name = models.CharField(max_length=255)
+    path = models.FileField(upload_to=document_path, default='')  # Proporciona un valor predeterminado
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    unique_code = models.CharField(max_length=50, unique=True)
+    loading_date = models.DateTimeField(auto_now_add=True)
+    auth_user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
